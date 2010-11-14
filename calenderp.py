37d5557a1b68b93b95332a6ca3cff3de42cad3ce
David Barker <kzar@kzar.co.uk>
2010-11-14 20:45:23 +0000

    Forgot to import new errors file..

--- calenderp.py
+++ calenderp.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, sys, re, facebook, logging, config, translations, traceback
+import os, sys, re, facebook, logging, config, translations, traceback, errors
 from google.appengine.api import users, urlfetch
 from google.appengine.api.labs import taskqueue
 from google.appengine.ext import db
