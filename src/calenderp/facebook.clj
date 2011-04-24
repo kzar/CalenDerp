2b29f8234110b4c74ae452329db91ee8175a8892
David Barker <kzar@kzar.co.uk>
2011-04-24 15:25:56 +0100

    More baby steps, got signed_request being properly decoded and running a facebook query using the token it contains.

--- src/calenderp/facebook.clj
+++ src/calenderp/facebook.clj
@@ -1,7 +1,9 @@
 (ns calenderp.facebook
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clj-facebook-graph.client :as client])
   (:use [clojure.contrib.json :only [read-json]]
-        [calenderp.config.config])
+        [calenderp.config.config]
+        [clj-facebook-graph.auth :only [facebook-auth-url with-facebook-auth]])
   (:import [org.apache.commons.codec.binary Base64]
            [javax.crypto Mac]
            [javax.crypto.spec SecretKeySpec]))
@@ -19,17 +21,26 @@
   "Decodes a base64 string, convenience wrapper around Java library."
   (String. (Base64/decodeBase64 base64)))
 
+(defn strtr [value from to]
+  "My take on PHP's strtr function."
+  ((apply comp (map (fn [a b] #(.replace % a b)) from to))
+   value))
+
 (defn decode-signed-request [signed-request key]
   "Takes a Facebook signed_request parameter and the applications secret
   key and returns a payload hash or nil if there was a problem."
   (when (and signed-request key)
     (let [[signiture payload] (str/split signed-request #"\.")
-          signiture (str (str/replace signiture #"[_-]" "/") "=")]
+          signiture (str (strtr signiture "-_" "+/") "=")]
       (when (= signiture (hmac-sha-256 key payload))
         (read-json (base64-decode payload))))))
+
+(defn friends [{token :oauth_token}]
+  (with-facebook-auth {:access-token token}
+    (client/get [:me :friends] {:extract :data})))
 
 (defn fb-auth-status [{signed-request "signed_request"}]
   (let [payload (decode-signed-request signed-request FACEBOOK-APP-SECRET)]
     (if payload
-      {:connected? true :payload payload}
+      {:connected? true :payload payload :friends (friends payload)}
       {:connected? false})))
--- test/calenderp/test/facebook_test.clj
+++ test/calenderp/test/facebook_test.clj
@@ -1,10 +1,15 @@
-(ns calenderp.test.facebook
+(ns calenderp.test.facebook-test
   (:use [calenderp.facebook])
   (:use [clojure.test]))
 
 (deftest hmac-sha-256-test
   (let [example "Mvq8XaiwC0gceHL5+4GF0xBeYZJu1uWk7bX5WQ6mvQ8="]
     (is (= example (hmac-sha-256 "test" "example message")) "Hashes correctly")))
+
+(deftest strtr-test
+  (is (= "dave/bob+test" (strtr "dave_bob-test" "-_" "+/")) "strtr works as we use it.")
+  (is (= "fr3d-jam3s" (strtr "fred james" " e" "-3")) "strtr works for other values")
+  (is (= "dave" (strtr "dave" "3" "1")) "strtr doesn't mess with other things"))
 
 (deftest decode-signed-request-test
   (let [example "vlXgu64BQGFSQrY0ZcJBZASMvYvTHu9GQ0YM9rjPSso.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsIjAiOiJwYXlsb2FkIn0"]
