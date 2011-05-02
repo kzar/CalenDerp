f5517556ed03b8c73f8fe45c5104c7d7a57091c4
David Barker <kzar@kzar.co.uk>
2011-05-02 22:40:16 +0100

    Got rid of signed_request decoding code as it's been moved to the facebook library.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [net.cgrand/moustache "1.0.0"]
                  [enlive "1.0.0-SNAPSHOT"]
-                 [org.clojars.charles-stain/clj-facebook-graph "0.1.0"]
+                 [clj-facebook-graph "0.2.0"]
                  [clj-time "0.3.0"]]
   :dev-dependencies [[appengine-magic "0.4.1"]
                      [swank-clojure "1.3.0-SNAPSHOT"]
--- src/calenderp/facebook.clj
+++ src/calenderp/facebook.clj
@@ -5,44 +5,13 @@
         [calenderp.config.config]
         [calenderp.utils :only [ignore-exceptions]]
         [clj-facebook-graph.auth :only [facebook-auth-url with-facebook-auth]]
-        [clj-time.format :only [parse formatter]])
+        [clj-time.format :only [parse formatter]]
+        [clj-facebook-graph.auth :only [decode-signed-request]])
   (:import [org.apache.commons.codec.binary Base64]
            [javax.crypto Mac]
            [javax.crypto.spec SecretKeySpec]))
 
 (def facebook-birthday-formatter (formatter "MM/dd/yyyy"))
-
-(defn hmac-sha-256
-  "Returns a HMAC-SHA256 hash of the provided data."
-  [^String key ^String data]
-  (let [hmac-key (SecretKeySpec. (.getBytes key) "HmacSHA256")
-        hmac (doto (Mac/getInstance "HmacSHA256") (.init hmac-key))]
-    (String. (org.apache.commons.codec.binary.Base64/encodeBase64
-              (.doFinal hmac (.getBytes data)))
-             "UTF-8")))
-
-(defn base64-decode
-  "Decodes a base64 string, convenience wrapper around Java library."
-  [base64]
-  (String. (Base64/decodeBase64 base64)))
-
-(defn strtr
-  "My take on PHP's strtr function."
-  [value from to]
-  ((apply comp (map (fn [a b] #(.replace % a b)) from to))
-   value))
-
-(defn decode-signed-request
-  "Takes a Facebook signed_request parameter and the applications secret
-  key and returns a payload hash or nil if there was a problem."
-  [signed-request key]
-  (when (and signed-request key
-             (re-matches #"^[^\.]+\.[^\.]+$" signed-request))
-    (let [[signiture payload] (str/split signed-request #"\.")
-          signiture (str (strtr signiture "-_" "+/") "=")]
-      (when (= signiture (hmac-sha-256 key payload))
-        (read-json (base64-decode payload))))))
-
 ; Decode event date - (parse "2011-07-17T01:00:00+0000")
 ; Decode birthday date - (parse facebook-birthday-formatter "04/14/1984")
 
